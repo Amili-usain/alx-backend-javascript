47cee163d92f2646c755b1bf2f69eecf7e04b9a6
Amili-usain <amilihussein073@gmail.com>
2023-04-20 11:51:27 +0000

    mandatory task 7 & 8

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+}
+
+Airport.prototype.toString = function airportToString() {
+  return `[object ${this._code}]`;
+};
