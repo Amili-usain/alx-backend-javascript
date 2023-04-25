c474ca8bab74b2e1234ac9596e11c610385a012d
Amili-usain <amilihussein073@gmail.com>
2023-04-25 08:04:29 +0000

    Bug fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,18 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  // Creates a new ArrayBuffer with the specified length.
-  const buffer = new ArrayBuffer(length);
-
-  // Checks whether the specified position is within the range of the ArrayBuffer.
   if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+    throw Error('Position outside range');
   }
-
-  // Creates a new Int8Array view of the ArrayBuffer.
-  const int8Array = new Int8Array(buffer);
-
-  // Sets the specified value at the specified position.
-  int8Array[position] = value;
-
-  // Returns the modified ArrayBuffer.
-  return buffer;
+  const newBuffer = new ArrayBuffer(length);
+  const int8view = new Int8Array(newBuffer, 0, length);
+  int8view.set([value], position);
+  return new DataView(newBuffer);
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,10 @@
 export default function cleanSet(set, startString) {
-  // If startString is falsy, return an empty string
-  if (!startString) {
-    return '';
-  }
-
-  // Initialize an empty array to store matching elements
-  const result = [];
-
-  // Iterate over the Set using a for...of loop
-  for (const element of set) {
-    // If the element starts with the startString, removes the startString and
-  // adds the resulting substring to the result array
-    if (element.startsWith(startString)) {
-      result.push(element.slice(startString.length));
+  const setRet = [];
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      setRet.push(s.slice(startString.length));
     }
-  }
-
-  // Joins the result array with hyphens and return the resulting string
-  return result.join('-');
+  });
+  return setRet.join('-');
 }
