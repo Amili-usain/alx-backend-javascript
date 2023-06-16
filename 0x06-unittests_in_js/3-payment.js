e23807b20b4df9c2f37e5762b51a224199edd7c6
Amili-usain <amilihussein073@gmail.com>
2023-06-16 19:24:27 +0000

    Bug fix

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,10 +1,8 @@
-const Utils = require('./utils.js');
+const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount) {
-  console.log(`Payment request for ${totalAmount}`);
-  const amount = Utils.calculateNumber(totalAmount, 0.3);
-  console.log(`Amount after calculation: ${amount}`);
-  // Send the payment request to the API...
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
 }
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,20 +1,16 @@
 const sinon = require('sinon');
+const Utils = require('./utils');
 const { expect } = require('chai');
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber and console.log with correct values', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(5);
-    const consoleLogSpy = sinon.spy(console, 'log');
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
-    sendPaymentRequestToApi(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.callCount).to.be.equal(1);
 
-    expect(calculateNumberStub.calledOnceWithExactly(10, 0.3)).to.be.true;
-    expect(consoleLogSpy.calledWithExactly('Payment request for 10')).to.be.true;
-    expect(consoleLogSpy.calledWithExactly('Amount after calculation: 5')).to.be.true;
-
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    calculateNumberSpy.restore();
   });
 });
